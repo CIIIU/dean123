ca478cc59c4fc5b8d11fdffc1cf8558d7fc943ee
CIIIU <95975022+CIIIU@users.noreply.github.com>
2023-11-17 01:44:49 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -1,57 +1,48 @@
-from flask import Flask, render_template
+import os.path
 
-from view.Admin.account_view import AccountView
-from view.Admin.blog_view import BlogView
-
-app = Flask(__name__)
-
-
-@app.route('/')
-def index():  # put application's code here
-    return render_template('client/index.html')
-
-
-@app.route('/donate')
-def donate():  # put application's code here
-    return render_template('client/donate.html')
-
-
-@app.route('/aboutus')
-def aboutus():  # put application's code here
-    return render_template('client/aboutus.html')
-
-
-@app.route('/contact')
-def contact():  # put application's code here
-    return render_template('client/contact_index.html')
-
-
-@app.route('/blog')
-def blog():  # put application's code here
-    return render_template('client/ex.html')
-
-
-
-from flask import Flask
+from flask import Flask, render_template, redirect, url_for, request
 from flask_sqlalchemy import SQLAlchemy
-from flask_admin import Admin
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+from flask_admin import Admin, AdminIndexView, expose
 from flask_admin.contrib.sqla import ModelView
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import relationship, backref
+from flask_admin.form import rules
 
+from view.base_model_view import BaseModelView
 
 USERNAME_DB = 'root'
 PASSWORD_DB = '123456'
 NAME_DB = 'dean'
 IP_DB = 'localhost'
 
+app = Flask(__name__)
+basedir = os.path.abspath(os.path.dirname(__file__))
 app.config["SQLALCHEMY_DATABASE_URI"] = \
     str.format(f"mysql+pymysql://{USERNAME_DB}:{PASSWORD_DB}@{IP_DB}/{NAME_DB}?charset=utf8mb4")
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
-app.config["FLASK_ADMIN_FLUID_LAYOUT"] = True
-app.secret_key = b'21137afa59a4dd08b708dcf106c724f9'
-db = SQLAlchemy(app=app)
+app.config['SECRET_KEY'] = 'lsdhflkjslkfjjeijfwef'
+db = SQLAlchemy(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
 
 
-class AdminModel(db.Model):
+class HomeView(AdminIndexView):
+    def is_visible(self):
+        return not current_user.is_authenticated
+
+    @expose('/')
+    def index(self):
+        return self.render('html/login.html')
+
+
+admin = Admin(
+    app
+    # index_view=HomeView()
+    # url='/admin'
+)
+
+
+class AdminModel(UserMixin, db.Model):
     __tablename__ = 'adminn'
 
     admin_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
@@ -63,32 +54,206 @@ class AdminModel(db.Model):
     birthdate = db.Column(db.DateTime)
     status = db.Column(db.String(50), default=None)
 
+    def get_id(self):
+        return self.admin_id
+
     def __str__(self):
         return self.name
 
 
-# class BlogModel(db.Model):
-#     __tablename__ = 'blog'
-#
-#     blog_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-#     admin_id = db.Column(db.Integer, db.ForeignKey('adminn.admin_id'), nullable=False)
-#     title = db.Column(db.String(50), nullable=False)
-#     datecre = db.Column(db.TIMESTAMP, server_default=db.func.current_timestamp(), nullable=False)
-#     image = db.Column(db.String(200), nullable=False)
-#     content = db.Column(db.Text, nullable=False)
-#     cateblog_id = db.Column(db.Integer, db.ForeignKey('cateblog.cateblog_id'), nullable=False)
-#
-#     admin = db.relationship('AdminModel', backref=db.backref('blogs', lazy=True))
-#     cateblog = db.relationship('CateblogModel', backref=db.backref('blogs', lazy=True))
-#
-#     def __str__(self):
-#         return self.title
+class CateBlogModel(db.Model):
+    __tablename__ = 'cateblog'
+
+    cateblog_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(50), nullable=False)
+    description = db.Column(db.String(200), nullable=False)
 
 
-admin = Admin(app, name='IGO')
-admin.add_view(AccountView(AdminModel, db.session, name='Tài khoản'))
-# admin.add_view(BlogView(BlogModel, db.session, name='Blog'))
+class BlogModel(db.Model):
+    __tablename__ = 'blog'
+
+    blog_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    admin_id = db.Column(db.Integer, nullable=True)
+    title = db.Column(db.String(50), nullable=False)
+    datecre = db.Column(db.TIMESTAMP, server_default='CURRENT_TIMESTAMP', nullable=False)
+    image = db.Column(db.String(200), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    cateblog_id = db.Column(db.Integer, nullable=False)
+    link = db.Column(db.Text)
+
+    # # Define relationships
+    # admin = relationship('AdminModel', backref='blogs')
+    # category = relationship('CateBlog', backref='blogs')
+
+
+class CommentModel(db.Model):
+    __tablename__ = 'comment'
+
+    cmt_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(50), nullable=False)
+    content = db.Column(db.String(200), nullable=False)
+    datecre = db.Column(db.TIMESTAMP, server_default='CURRENT_TIMESTAMP', nullable=False)
+    blog_id = db.Column(db.Integer, db.ForeignKey('blog.blog_id', ondelete='CASCADE'))
+#
+#     # Define relationship
+#     blog = relationship('Blog', backref='comments')
+
+
+class UserContactModel(db.Model):
+    __tablename__ = 'user_contact'
+
+    user_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    email = db.Column(db.String(50), nullable=False)
+
+
+class CateProjectModel(db.Model):
+    __tablename__ = 'cateproject'
+
+    cateproj_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(50), nullable=False)
+    description = db.Column(db.Text)
+
+
+class ProjectModel(db.Model):
+    __tablename__ = 'project'
+
+    project_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    title = db.Column(db.String(50), nullable=False)
+    subTitle = db.Column(db.String(50), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    datecre = db.Column(db.TIMESTAMP, server_default='CURRENT_TIMESTAMP', nullable=False)
+    admin_id = db.Column(db.Integer, nullable=False)
+    photoURL = db.Column(db.String(100), nullable=False)
+    cateproj_id = db.Column(db.Integer, nullable=False)
+
+
+
+@login_manager.user_loader
+def load_user(admin_id):
+    return AdminModel.query.get(int(admin_id))
+
+
+# class MyModelView(ModelView):
+#     def is_accessible(self):
+#         return current_user.is_authenticated
+#
+#     def inaccessible_callback(self, name, **kwargs):
+#         # Redirect to login page if user is not logged in
+#         return redirect(url_for('login'))
+
+
+@app.route('/index')
+def index():
+    return render_template('html/index.html')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login_admin():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        user = AdminModel.query.filter_by(username=username).first()
+        if user and password == user.password:
+            login_user(user)
+    return redirect('/admin')
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+
+class CateBlogView(BaseModelView):
+    can_edit = False
+    can_create = False
+    can_delete = False
+
+
+class UserContactView(BaseModelView):
+    can_delete = True
+    can_create = False
+    can_edit = False
+
+
+class CateProjectView(BaseModelView):
+    pass
+
+
+class CommentView(BaseModelView):
+    column_list = ['cmt_id', 'username', 'email', 'content', 'datecre', 'blog_id',]
+    can_edit = False;
+
+    def is_visible(self):
+        return True
+
+
+class ProjectView(BaseModelView):
+    column_list = ['project_id', 'title', 'subTitle', 'content', 'datecre', 'admin_id', 'photoURL', 'cateproj_id']
+
+    # Add the custom view to the menu
+    def is_visible(self):
+        return True
+
+
+class BlogView(BaseModelView):
+    column_list = ['blog_id','admin_id','title', 'datecre', 'image', 'content', 'cateblog_id','link' ]
+    column_searchable_list = ['title']
+
+    def is_visible(self):
+        return True
+
+    # form_rules = [
+    #     rules.FieldSet(('title', 'datecre', 'image', 'content', 'link','admin_id'), 'Thông tin blog'),
+    #     # rules.FieldSet(('admin_id', 'cateblog_id'), 'Các thông tin khác')
+    # ]
+
+
+# Other routes and functions...
+admin.add_view(BaseModelView(AdminModel, db.session, name="Quản trị viên"))
+admin.add_view(CateBlogView(CateBlogModel, db.session, name="Loại blog"))
+admin.add_view(BlogView(BlogModel, db.session, name="Blog"))
+admin.add_view(CommentView(CommentModel, db.session, name="Bình luận"))
+admin.add_view(CateProjectView(CateProjectModel, db.session, name="Loại Project"))
+admin.add_view(ProjectView(ProjectModel, db.session, name="Project"))
+admin.add_view(UserContactView(UserContactModel, db.session, name="Thông tin liên hệ"))
+
+
+@app.route('/blog')
+def blog():
+    blog_posts = BlogModel.query.all()
+
+    return render_template('html/blog.html', blog_posts=blog_posts)
+
+
+@app.route('/project')
+def project():
+    projects = ProjectModel.query.all()
+
+    return render_template('html/project.html', projects=projects)
+
+
+@app.route('/')
+def home():
+    return render_template('html/home.html')
+
+
+
+@app.route('/subscribe', methods=['POST'])
+def subscribe():
+    if request.method == 'POST':
+        email = request.form['newsletter1']
+        new_usercontact = UserContactModel(email=email)
+
+        try:
+            db.session.add(new_usercontact)
+            db.session.commit()
+        except:
+            db.session.rollback()
+        return render_template('html/subscribe.html')
 
 if __name__ == '__main__':
-    app.run(port=5002)
-
+    db.create_all()
+    app.run(debug=True)
